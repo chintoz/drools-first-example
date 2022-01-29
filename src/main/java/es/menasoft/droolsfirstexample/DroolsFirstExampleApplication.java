151b4fd4a7b423b44f5f78cf1a88bd514e17bd65
Jacinto Jesús Mena Lomeña <jacintomena@gmail.com>
2022-01-29 23:38:05 +0100

    Initial commit.

---
+++ src/main/java/es/menasoft/droolsfirstexample/DroolsFirstExampleApplication.java
@@ -0,0 +1,13 @@
+package es.menasoft.droolsfirstexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsFirstExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsFirstExampleApplication.class, args);
+    }
+
+}
